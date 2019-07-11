1908ea4f3b8709bbc01fcdd92cda2f0a38e0e0e1
Sutthirath Sean Vilaysane <seanvilaysane@gmail.com>
2019-07-10 23:11:35 -0700

    Lyrics added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
 
 import { Provider } from "./context";
 
@@ -16,6 +17,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
